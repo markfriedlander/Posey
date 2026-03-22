1acedb20d64d0b17fd3f8b20251f34beb60785cc
Mark Friedlander <markfriedlander@yahoo.com>
2026-03-22 13:19:27 -0700

    Initial Commit

---
+++ Posey/PoseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseyApp.swift
+//  Posey
+//
+//  Created by Mark Friedlander on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PoseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
